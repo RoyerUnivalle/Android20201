59ca313be115b5957005e754c9a07a1ae963ca8f
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2020-05-06 11:03:12 -0500

    se agrega tarea en segundo plano con asyntask

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -3,15 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
     String name,passwd;
     TextView showData, tvContador;
     int contador=0;
+    Button pintar;
+    Pintar Obj = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,7 @@ public class Home extends AppCompatActivity {
         //enlace
         showData = findViewById(R.id.tvData);
         tvContador = findViewById(R.id.tvContador);
+        pintar = findViewById(R.id.btnPintar);
         Bundle data = getIntent().getExtras();
         name = data.getString("name");
         passwd = data.getString("passwd");
@@ -28,6 +35,7 @@ public class Home extends AppCompatActivity {
             contador = savedInstanceState.getInt("contador");
             tvContador.setText("Contador: "+ contador);
         }
+        Obj = new Pintar();
     }
 
     public void backToLogin(View g) {
@@ -41,9 +49,71 @@ public class Home extends AppCompatActivity {
         tvContador.setText("Contador: "+ contador);
     }
 
+    public void pintar(View q) throws InterruptedException {
+        Obj = new Pintar();
+        Obj.execute("hola");
+        /*if(Obj != null){
+            Obj.execute("hola");
+        }else {
+            Obj.cancel(true);
+            Obj.execute("hola");
+        }*/
+        /*for(int i= 0; i<=20; i++){
+            Thread.sleep(1000);
+            pintar.setBackgroundColor(Color.rgb(getRamdon(),getRamdon(),getRamdon()));
+        }*/
+    }
+
+    public int getRamdon(){
+        return  (int)(Math.random()*255 + 1);   // Esto da valores entre 1 y 255
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("contador",contador);
     }
+
+    class  Pintar extends AsyncTask<String,Integer,String>{
+        String mensaje;
+        @Override
+        protected String doInBackground(String... msj) { // No acceso a elementos de la UI
+            mensaje = msj[0];
+            for(int i= 0; i<=20; i++){
+                if(!isCancelled()){
+                    try {
+                        Thread.sleep(1000);
+                        publishProgress(i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String aVoid) {
+            Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG);
+            super.onPostExecute(aVoid);
+        }
+        @Override // segundo parametro de la calse
+        protected void onProgressUpdate(Integer... numero) { // si tiene acceso a la UI
+            pintar.setBackgroundColor(Color.rgb(getRamdon(),getRamdon(),getRamdon()));
+            pintar.setText("i: "+ numero[0]);
+            super.onProgressUpdate(numero);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            Toast.makeText(getApplicationContext(), "Hilo Cancelado", Toast.LENGTH_LONG);
+            Obj = null;
+            super.onCancelled(s);
+        }
+
+        public int getRamdon(){
+            return  (int)(Math.random()*255 + 1);   // Esto da valores entre 1 y 255
+        }
+    }
+
 }
