aaa619d9e5f4ea2f55705c806e8b52c7feb6d763
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2020-05-12 08:15:12 -0500

    se entre avance en volley con exito y httpurlconnection con error 404

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -11,6 +11,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Home extends AppCompatActivity {
 
     String name,passwd;
@@ -18,6 +33,9 @@ public class Home extends AppCompatActivity {
     int contador=0;
     Button pintar;
     Pintar Obj = null;
+    String DATA_URL = "https://invessoft.com/api/eventos/1";
+    Consultar obj2 = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +67,23 @@ public class Home extends AppCompatActivity {
         tvContador.setText("Contador: "+ contador);
     }
 
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void pintar(View q) throws InterruptedException {
         Obj = new Pintar();
         Obj.execute("hola");
@@ -64,6 +99,38 @@ public class Home extends AppCompatActivity {
         }*/
     }
 
+    public void consultar(View q){
+        obj2 = new Consultar();
+        obj2.execute();
+    }
+
+    public void consultar2(View q){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, DATA_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // https://docs.oracle.com/javaee/7/api/javax/json/JsonArray.html
+                        // Display the first 500 characters of the response string.
+                        System.out.println("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("error" + error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+        //obj2.execute();
+    }
+
     public int getRamdon(){
         return  (int)(Math.random()*255 + 1);   // Esto da valores entre 1 y 255
     }
@@ -74,6 +141,7 @@ public class Home extends AppCompatActivity {
         outState.putInt("contador",contador);
     }
 
+    // Clase
     class  Pintar extends AsyncTask<String,Integer,String>{
         String mensaje;
         @Override
@@ -113,6 +181,46 @@ public class Home extends AppCompatActivity {
 
         public int getRamdon(){
             return  (int)(Math.random()*255 + 1);   // Esto da valores entre 1 y 255
+        }
+    }
+
+    class Consultar extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL urlConexion = null;
+            try {
+                urlConexion = new URL(DATA_URL);
+                // primer paso segun https://developer.android.com/reference/java/net/HttpURLConnection.html
+                HttpURLConnection conexion = (HttpURLConnection) urlConexion.openConnection();
+                //segundo paso -headers
+                conexion.setDoOutput(true);
+                conexion.setDoInput(true);
+                conexion.setRequestMethod("GET");
+                conexion.setUseCaches(false);
+                conexion.setConnectTimeout(10000);
+                conexion.setReadTimeout(10000);
+                conexion.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+                // conexion.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+                conexion.connect();
+                // debemos consultar si esta con estado 200
+                int connectionStatus = conexion.getResponseCode();
+                if(connectionStatus == HttpURLConnection.HTTP_OK){// LA CONEXION FUE EXITOSA
+                    System.out.println("HOLA RESPUESTA "+conexion.getInputStream());
+                    //conexion.getInputStream();
+                }else {
+                    System.out.println(connectionStatus);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
         }
     }
 
